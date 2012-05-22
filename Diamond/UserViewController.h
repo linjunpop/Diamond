93ccdb55445e2c602448a189bb26bb9147459fd1
Jun Lin <linjunpop@gmail.com>
2012-05-22 20:02:18 +0800

    Implemented a simple users tableview.

---
+++ Diamond/UserViewController.h
@@ -0,0 +1,18 @@
+//
+//  UserViewController.h
+//  Diamond
+//
+//  Created by Jun Lin on 5/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property NSMutableArray *users;
+
+- (IBAction)refresh:(id)sender;
+- (Boolean)fetch;
+
+@end
---
+++ Diamond/UserViewController.m
@@ -0,0 +1,102 @@
+//
+//  UserViewController.m
+//  Diamond
+//
+//  Created by Jun Lin on 5/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserViewController.h"
+
+@interface UserViewController ()
+
+@end
+
+@implementation UserViewController
+@synthesize tableView = _tableView;
+@synthesize users = _users;
+
+#pragma mark - View Lifecycle
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [self setTableView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+#pragma mark - UITableViewDataSource methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.users count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if(!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                       reuseIdentifier:nil];
+    }
+    
+    NSDictionary *userInfo = [self.users objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = [userInfo objectForKey:@"login"];
+    
+    return cell;
+}
+
+- (IBAction)refresh:(id)sender {
+    if (self.fetch) {
+        [self.tableView reloadData];
+    }
+}
+
+- (Boolean)fetch {
+    NSString* strUrl=@"http://ruby-china.org/api/users"; //it return json format string
+    
+    NSURL *url=[NSURL URLWithString:strUrl];
+	NSMutableURLRequest  *request=[[NSMutableURLRequest alloc] initWithURL:url];      
+    NSError *err=nil;    
+    NSData *jsonData=[NSURLConnection sendSynchronousRequest:request
+                                           returningResponse:nil
+                                                       error:&err];
+    
+    NSError *e = nil;
+    self.users = [NSJSONSerialization JSONObjectWithData: jsonData
+                                                     options: NSJSONReadingMutableContainers
+                                                       error: &e];
+    if(self.users) {
+        return true;
+    } else {
+        return false;
+    }
+}
+@end
